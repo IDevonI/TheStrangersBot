8e3c9d9c38ccdbd407442af1d83a443ba50bcb8a
Devon <devindomt@gmail.com>
2021-01-09 22:52:55 +0100

    token to getenv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,9 @@ import javax.security.auth.login.LoginException;
 public class Main {
     public static void main(String[] args) {
         JDA jda;
-        //String token=System.getenv("TOKEN");
+        String token=System.getenv("TOKEN");
         try {
-            jda = JDABuilder.createDefault("Nzk3MTc5MTAxNTYyNTM1OTQ2.X_is5A.lt2AMPrWGZWGPXEyenbMqMHaVfQ").enableIntents(GatewayIntent.GUILD_PRESENCES).build();
+            jda = JDABuilder.createDefault(token).enableIntents(GatewayIntent.GUILD_PRESENCES).build();
             jda.addEventListener(new Creator());
         } catch (LoginException e) {
             e.printStackTrace();
